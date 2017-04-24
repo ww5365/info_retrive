4256b6df47bac60ffcfdb8c54886bd49a7c752a0
ww5365 <ww5365@163.com>
2017-04-24 16:16:12 +0800

    src

---
+++ src/info_retrive.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : info_retrive.cpp
+// Author      : ww
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "info retrive" << endl; // prints info retrive
+	return 0;
+}
