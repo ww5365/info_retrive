4d01359d63927c186b77b9c4545a28fce9e9a61c
ww5365 <ww5365@163.com>
2017-04-28 16:10:37 +0800

    just test

--- src/guide_book/3.cpp
+++ src/guide_book/3.cpp
@@ -8,9 +8,23 @@
 #include "all.h"
 #include <iostream>
 
+
+
 int test_3(){
 
     std::cout<< "test 3 test 3" <<std::endl;
+
+}
+
+
+
+
+/*
+ * 功能：计算两个字符串：str1 和 str2的编辑距离
+ *
+ */
+
+int edit_distance(std::string str1,std::string str2){
 
 }
 
